61d6652e36132b628401c1ea6da53da57faaa055
Chris <ysm0622@gmail.com>
2022-06-17 20:17:20 +0200

    dev

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ import { serve } from 'https://deno.land/std@0.144.0/http/server.ts'
 
 serve((req) => {
   console.log(req)
-  return new Response('Hello World\n')
+  return new Response('Hello World!\n')
 })
