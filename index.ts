33d6f84bc15d5385fb3325b789fa056864a962d0
Chris <ysm0622@gmail.com>
2022-08-25 09:19:32 +0700

    change link

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { serve } from 'https://deno.land/std/http/mod.ts'
 
 serve(
   () => {
-    return Response.redirect('https://discord.com/invite/4y93ujhH', 307)
+    return Response.redirect('https://discord.com/invite/5HwyPHkaWq', 307)
   },
   { port: 3000 },
 )
